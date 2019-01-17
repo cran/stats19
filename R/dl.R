453f3670546cecc23db29d6df3b975cfdfb44c36
Robin Lovelace <rob00x@gmail.com>
2019-01-17 22:10:17 +0000

    version 0.1.1

---
+++ R/dl.R
@@ -0,0 +1,92 @@
+#' Download STATS19 data for a year or range of two years.
+#'
+#' @section Details:
+#' This function downloads and unzips UK road crash data.
+#' It results in unzipped .csv files that are put
+#' in the temporary directory specified by `tempdir()` or provided `data_dir`.
+#'
+#' The file downloaded would be for a specific year (e.g. 2017).
+#' It could also be a file containing data for a range of two (e.g. 2005-2014).
+#'
+#' The `dl_*` functions can download many MB of data so ensure you
+#' have a sufficient internet access and hard disk space.
+#'
+#' @seealso [get_stats19()]
+#'
+#' @param file_name The file name (DfT named) to download.
+#' @param year Single year for which file is to be downloaded.
+#' @param type One of 'Accidents', 'Casualties', 'Vehicles'; defaults to 'Accidents'.
+#' Or any variation of to search the file names with such as "acc" or "accid".
+#' @param data_dir Parent directory for all downloaded files. Defaults to `tempdir()`.
+#' @param ask Should you be asked whether or not to download the files? `TRUE` by default.
+#'
+#' @export
+#' @examples
+#' \donttest{
+#' dl_stats19(year = 2017) # interactively select files...
+#' # now you can read-in the data
+#' dl_stats19(year = 2009)
+#' dl_stats19(year = 2009, type = "casualties")
+#' dl_stats19(type = "casualties")
+#' dl_stats19(year = 1985)
+#' }
+dl_stats19 = function(year = NULL,
+                      type = NULL,
+                      data_dir = tempdir(),
+                      file_name = NULL,
+                      ask = TRUE
+                      ) {
+  if(!is.null (year)) {
+    year = check_year(year)
+  }
+  if(is.null(file_name)) {
+    fnames = find_file_name(years = year, type = type)
+    nfiles_found = length(fnames)
+    many_found = nfiles_found > 1
+    if(many_found) {
+      if(interactive()) {
+        fnames = select_file(fnames)
+      } else {
+        message("More than one file found, selecting the first.")
+        fnames = fnames[1]
+      }
+    }
+    zip_url = get_url(fnames)
+  } else {
+    many_found = FALSE
+    fnames = file_name
+    nfiles_found = length(fnames)
+    zip_url = get_url(file_name = file_name)
+  }
+
+  message("Files identified: ", paste0(fnames, "\n"))
+  message(paste0("   ", zip_url, collapse = "\n"))
+  if (!dir.exists(data_dir)) {
+    dir.create(data_dir, recursive = TRUE)
+  }
+  exdir = sub(".zip", "", fnames)
+  destfile = file.path(data_dir, paste0(exdir, ".zip"))
+  data_already_exists = file.exists(destfile)
+  if(data_already_exists) {
+    message("Data already exists in data_dir, not downloading")
+  } else {
+    if(interactive() & !many_found) {
+      if(ask) {
+        resp = readline(phrase())
+      } else {
+        resp = ""
+      }
+      if (resp != "" & !grepl(pattern = "yes|y", x = resp, ignore.case = TRUE)) {
+        stop("Stopping as requested", call. = FALSE)
+      }
+    }
+    message("Attempt downloading from: ")
+    utils::download.file(zip_url, destfile = destfile)
+  }
+
+  f = file.path(destfile, utils::unzip(destfile, list = TRUE)$Name)
+  utils::unzip(destfile, exdir = file.path(data_dir, exdir))
+
+  message("Data saved at ", sub(".zip", "",f))
+
+}
---
+++ R/format.R
@@ -0,0 +1,131 @@
+#' Format STATS19 'accidents' data
+#'
+#' @section Details:
+#' This is a helper function to format raw STATS19 data
+#'
+#' @param x Data frame created with `read_accidents()`
+#' @export
+#' @examples
+#' \donttest{
+#' dl_stats19(year = 2017, type = "accident")
+#' x = read_accidents(year = 2017)
+#' crashes = format_accidents(x)
+#' }
+#' @export
+format_accidents = function(x) {
+  format_stats19(x, type = "accidents")
+}
+#' Format STATS19 casualties
+#'
+#' @section Details:
+#' This function formats raw STATS19 data
+#'
+#' @param x Data frame created with `read_casualties()`
+#' @export
+#' @examples
+#' \donttest{
+#' dl_stats19(year = 2017, type = "casualties")
+#' x = read_casualties(year = 2017)
+#' casualties = format_casualties(x)
+#' }
+#' @export
+format_casualties = function(x) {
+  format_stats19(x, type = "casualties")
+}
+#' Format STATS19 vehicles data
+#'
+#' @section Details:
+#' This function formats raw STATS19 data
+#'
+#' @param x Data frame created with `read_vehicles()`
+#' @export
+#' @examples
+#' \donttest{
+#' dl_stats19(year = 2017, type = "vehicles")
+#' x = read_vehicles(year = 2017, format = FALSE)
+#' vehicles = format_vehicles(x)
+#' }
+#' @export
+format_vehicles = function(x) {
+  format_stats19(x, type = "vehicles")
+}
+
+format_stats19 = function(x, type) {
+  # rename colums
+  old_names = names(x)
+  new_names = format_column_names(old_names)
+  names(x) = new_names
+
+  # create lookup table
+  lkp = stats19_variables[stats19_variables$table == type,]
+
+  vkeep = new_names %in% stats19_schema$variable_formatted
+  vars_to_change = which(vkeep)
+
+  for(i in vars_to_change) {
+    lkp_name = lkp$column_name[lkp$column_name == new_names[i]]
+    lookup = stats19_schema[stats19_schema$variable_formatted == lkp_name, 1:2]
+    x[[i]] = lookup$label[match(x[[i]], lookup$code)]
+  }
+  x
+}
+
+#' Format column names of raw STATS19 data
+#'
+#' This function takes messy column names and returns clean ones that work well with
+#' R by default. Names that are all lower case with no R-unfriendly characters
+#' such as spaces and `-` are returned.
+#' @param column_names Column names to be cleaned
+#' @return Column names cleaned.
+#' @export
+#' @examples
+#' \donttest{
+#' crashes_raw = read_accidents(year = 2017)
+#' column_names = names(crashes_raw)
+#' column_names
+#' format_column_names(column_names = column_names)
+#' }
+format_column_names = function(column_names) {
+  x = tolower(column_names)
+  x = gsub(pattern = " ", replacement = "_", x = x)
+  x = gsub(pattern = "\\(|\\)", replacement = "", x = x)
+  x = gsub(pattern = "1st", replacement = "first", x = x)
+  x = gsub(pattern = "2nd", replacement = "second", x = x)
+  x = gsub(pattern = "-", replacement = "_", x = x)
+  x = gsub(pattern = "\\?", replacement = "", x)
+  x
+}
+
+#' Format convert STATS19 data into spatial (sf) object
+#'
+#' @param x Data frame created with `read_accidents()`
+#' @param lonlat Should the results be returned in longitude/latitude?
+#' By default `FALSE`, meaning the British National Grid (EPSG code: 27700)
+#' is used.
+#'
+#' @export
+#' @examples
+#' x_sf = format_sf(accidents_sample)
+#' sf:::plot.sf(x_sf)
+#' @export
+format_sf = function(x, lonlat = FALSE) {
+  n = names(x)
+  if(lonlat) {
+    coords = n[grep(pattern = "longitude|latitude",
+                    x = n,
+                    ignore.case = TRUE)]
+    coord_null = is.na(x[[coords[1]]] | x[[coords[2]]])
+    x = x[!coord_null, ]
+    message(sum(coord_null), " rows removed with no coordinates")
+    x_sf = sf::st_as_sf(x, coords = coords, crs = 4326)
+  } else {
+    coords = n[grep(pattern = "easting|northing",
+                    x = n,
+                    ignore.case = TRUE)]
+    coord_null = is.na(x[[coords[1]]] | x[[coords[2]]])
+    message(sum(coord_null), " rows removed with no coordinates")
+    x = x[!coord_null, ]
+    x_sf = sf::st_as_sf(x, coords = coords, crs = 27700)
+  }
+  x_sf
+}
